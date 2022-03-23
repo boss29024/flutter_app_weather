40ba69b2fbabb941f58658019ca94de3cce79a63
boss29024 <boss29024@gmail.com>
2022-03-23 16:54:21 +0700

    home

--- lib/page/HomePage.dart
+++ lib/page/HomePage.dart
@@ -12,18 +12,37 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    final heightDevice = MediaQuery.of(context).size.height;
+    final widthDevice = MediaQuery.of(context).size.width;
     return Scaffold(
       body: Container(
         padding: EdgeInsets.all(12),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
+            SizedBox(
+              height: (heightDevice / 100) * 2,
+            ),
             Container(
-              child: Row(
-                  children: [Icon(Icons.add), Text('บ้านหนองไม้แดง')]),
+              child: Row(children: [Icon(Icons.add), Text('บ้านหนองไม้แดง')]),
+            ),
+            SizedBox(
+              height: (heightDevice / 100) * 5,
             ),
             Center(
-              child: Column(children: [Text('31c'), Text('มีเมฆ')]),
+              child: Column(children: [
+                Text('31c'),
+                Text('มีเมฆ'),
+              ]),
+            ),
+            SizedBox(
+              height: (heightDevice / 100) * 5,
+            ),
+            Card(
+              child: Container(
+                  child: Row(
+                children: [],
+              )),
             )
           ],
         ),

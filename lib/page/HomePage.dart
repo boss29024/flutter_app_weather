0f890d7a2d0a44bd3a7e4d45db02309683174412
boss29024 <boss29024@gmail.com>
2022-03-24 12:38:44 +0700

    model

--- lib/page/HomePage.dart
+++ lib/page/HomePage.dart
@@ -15,6 +15,7 @@ class _HomePageState extends State<HomePage> {
     final heightDevice = MediaQuery.of(context).size.height;
     final widthDevice = MediaQuery.of(context).size.width;
     return Scaffold(
+      appBar: AppBar(title: Text('Weather App')),
       body: Container(
         padding: EdgeInsets.all(12),
         child: Column(
@@ -24,24 +25,48 @@ class _HomePageState extends State<HomePage> {
               height: (heightDevice / 100) * 2,
             ),
             Container(
-              child: Row(children: [Icon(Icons.add), Text('บ้านหนองไม้แดง')]),
+              child: Row(children: [
+                Icon(Icons.add),
+                SizedBox(
+                  width: (widthDevice / 100) * 25,
+                ),
+                Text(
+                  'บ้านหนองไม้แดง',
+                  style: TextStyle(fontSize: 24),
+                ),
+              ]),
             ),
             SizedBox(
-              height: (heightDevice / 100) * 5,
+              height: (heightDevice / 100) * 15,
             ),
             Center(
               child: Column(children: [
-                Text('31c'),
+                Text(
+                  '31c',
+                  style: TextStyle(fontSize: 108),
+                ),
                 Text('มีเมฆ'),
               ]),
             ),
             SizedBox(
               height: (heightDevice / 100) * 5,
             ),
+            Container(
+              child: Row(
+                children: [
+                  Column(
+                    children: [Text('ขณะนี้'), Text('31c')],
+                  )
+                ],
+              ),
+            ),
             Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+              ),
               child: Container(
-                  child: Row(
-                children: [],
+                  child: Column(
+                children: [Text('พยากรณ์ 5วัน')],
               )),
             )
           ],

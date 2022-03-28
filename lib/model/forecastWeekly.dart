aa6a983c3c1a7805061efa503a603902db8f0385
boss29024 <boss29024@gmail.com>
2022-03-28 17:05:17 +0700

    ForecastWeekly

---
+++ lib/model/forecastWeekly.dart
@@ -0,0 +1,259 @@
+class ForecastWeekly {
+  Current? current;
+  Forecast? forecast;
+  Location? location;
+
+  ForecastWeekly({
+    this.current,
+    this.forecast,
+    this.location,
+  });
+
+  ForecastWeekly.fromJson(Map<String, dynamic> json) {
+    current =
+        json['current'] != null ? new Current.fromJson(json['current']) : null;
+    forecast = json['forecast'] != null
+        ? new Forecast.fromJson(json['forecast'])
+        : null;
+    location = json['location'] != null
+        ? new Location.fromJson(json['location'])
+        : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    if (this.current != null) {
+      data['current'] = this.current!.toJson();
+    }
+    if (this.forecast != null) {
+      data['forecast'] = this.forecast!.toJson();
+    }
+    if (this.location != null) {
+      data['location'] = this.location!.toJson();
+    }
+    return data;
+  }
+}
+
+class Current {
+  int? lastUpdatedEpoch;
+  String? lastUpdated;
+  double? tempC;
+  double? tempF;
+  int? isDay;
+  Condition? condition;
+
+  Current({
+    this.lastUpdatedEpoch,
+    this.lastUpdated,
+    this.tempC,
+    this.tempF,
+    this.isDay,
+    this.condition,
+  });
+
+  Current.fromJson(Map<String, dynamic> json) {
+    lastUpdatedEpoch = json['last_updated_epoch'];
+    lastUpdated = json['last_updated'];
+    tempC = json['temp_c'];
+    tempF = json['temp_f'];
+    isDay = json['is_day'];
+    condition = json['condition'] != null
+        ? new Condition.fromJson(json['condition'])
+        : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['last_updated_epoch'] = this.lastUpdatedEpoch;
+    data['last_updated'] = this.lastUpdated;
+    data['temp_c'] = this.tempC;
+    data['temp_f'] = this.tempF;
+    data['is_day'] = this.isDay;
+    if (this.condition != null) {
+      data['condition'] = this.condition!.toJson();
+    }
+    return data;
+  }
+}
+
+class Location {
+  String? region;
+  String? country;
+  String? localtime;
+
+  Location({this.region, this.country, this.localtime});
+
+  Location.fromJson(Map<String, dynamic> json) {
+    region = json['region'];
+    country = json['country'];
+    localtime = json['localtime'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['region'] = this.region;
+    data['country'] = this.country;
+    data['localtime'] = this.localtime;
+    return data;
+  }
+}
+
+class Condition {
+  String? text;
+  String? icon;
+  int? code;
+
+  Condition({this.text, this.icon, this.code});
+
+  Condition.fromJson(Map<String, dynamic> json) {
+    text = json['text'];
+    icon = json['icon'];
+    code = json['code'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['text'] = this.text;
+    data['icon'] = this.icon;
+    data['code'] = this.code;
+    return data;
+  }
+}
+
+class Forecast {
+  List<Forecastday>? forecastday;
+
+  Forecast({this.forecastday});
+
+  Forecast.fromJson(Map<String, dynamic> json) {
+    if (json['forecastday'] != null) {
+      forecastday = <Forecastday>[];
+      json['forecastday'].forEach((v) {
+        forecastday!.add(new Forecastday.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    if (this.forecastday != null) {
+      data['forecastday'] = this.forecastday!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class Forecastday {
+  String? date;
+  int? dateEpoch;
+  Day? day;
+  List<Hour>? hour;
+
+  Forecastday({this.date, this.dateEpoch, this.day, this.hour});
+
+  Forecastday.fromJson(Map<String, dynamic> json) {
+    date = json['date'];
+    dateEpoch = json['date_epoch'];
+    day = json['day'] != null ? new Day.fromJson(json['day']) : null;
+    if (json['hour'] != null) {
+      hour = <Hour>[];
+      json['hour'].forEach((v) {
+        hour!.add(new Hour.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['date'] = this.date;
+    data['date_epoch'] = this.dateEpoch;
+    if (this.day != null) {
+      data['day'] = this.day!.toJson();
+    }
+    if (this.hour != null) {
+      data['hour'] = this.hour!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class Day {
+  double? maxtempC;
+  double? maxtempF;
+  double? mintempC;
+  double? mintempF;
+  double? avgtempC;
+  double? avgtempF;
+
+  Day({
+    this.maxtempC,
+    this.maxtempF,
+    this.mintempC,
+    this.mintempF,
+    this.avgtempC,
+    this.avgtempF,
+  });
+
+  Day.fromJson(Map<String, dynamic> json) {
+    maxtempC = json['maxtemp_c'];
+    maxtempF = json['maxtemp_f'];
+    mintempC = json['mintemp_c'];
+    mintempF = json['mintemp_f'];
+    avgtempC = json['avgtemp_c'];
+    avgtempF = json['avgtemp_f'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['maxtemp_c'] = this.maxtempC;
+    data['maxtemp_f'] = this.maxtempF;
+    data['mintemp_c'] = this.mintempC;
+    data['mintemp_f'] = this.mintempF;
+    data['avgtemp_c'] = this.avgtempC;
+    data['avgtemp_f'] = this.avgtempF;
+    return data;
+  }
+}
+
+class Hour {
+  int? timeEpoch;
+  String? time;
+  double? tempC;
+  double? tempF;
+  int? isDay;
+  Condition? condition;
+
+  Hour({
+    this.timeEpoch,
+    this.time,
+    this.tempC,
+    this.tempF,
+    this.isDay,
+    this.condition,
+  });
+
+  Hour.fromJson(Map<String, dynamic> json) {
+    timeEpoch = json['time_epoch'];
+    time = json['time'];
+    tempC = json['temp_c'];
+    tempF = json['temp_f'];
+    isDay = json['is_day'];
+    condition = json['condition'] != null
+        ? new Condition.fromJson(json['condition'])
+        : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['time_epoch'] = this.timeEpoch;
+    data['time'] = this.time;
+    data['temp_c'] = this.tempC;
+    data['temp_f'] = this.tempF;
+    data['is_day'] = this.isDay;
+    if (this.condition != null) {
+      data['condition'] = this.condition!.toJson();
+    }
+    return data;
+  }
+}

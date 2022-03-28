38df798e5e2994499bfc10b8be8153c20afc3436
boss29024 <boss29024@gmail.com>
2022-03-28 17:05:33 +0700

    ต่อapi

--- lib/page/HomePage.dart
+++ lib/page/HomePage.dart
@@ -1,4 +1,9 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app_weather_forecast/model/currentWeather.dart';
+import 'package:flutter_app_weather_forecast/model/forecastWeekly.dart';
+import 'package:flutter_app_weather_forecast/widget/cardHourly.dart';
+import 'package:flutter_app_weather_forecast/widget/cardWeekly.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key? key}) : super(key: key);
@@ -8,10 +13,45 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var dataCurrent = CurrentWeather();
+  var dataForecast = ForecastWeekly();
+  void getCurrentWeather() async {
+    var dio = Dio();
+    final response = await dio.get(
+        'http://api.weatherapi.com/v1/current.json?key=18d7b9d6de11454aaea54936222503&q=bangkok&aqi=yes');
+    if (response.statusCode == 200) {
+      setState(() {
+        dataCurrent = CurrentWeather.fromJson(response.data);
+      });
+    } else {
+      print('error');
+    }
+  }
+
+  Future<void> getForecast() async {
+    var dio = Dio();
+    final response = await dio.get(
+        'http://api.weatherapi.com/v1/forecast.json?key=18d7b9d6de11454aaea54936222503&q=bangkok&q=bangkok&days=7&aqi=yes&alerts=no');
+
+    if (response.statusCode == 200) {
+      setState(() {
+        dataForecast = ForecastWeekly.fromJson(response.data);
+      });
+    } else {
+      print('error');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentWeather();
+    getForecast();
+  }
+
   @override
   Widget build(BuildContext context) {
     final heightDevice = MediaQuery.of(context).size.height;
-    final widthDevice = MediaQuery.of(context).size.width;
 
     return Scaffold(
         appBar: AppBar(
@@ -30,12 +70,7 @@ class _HomePageState extends State<HomePage> {
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
               _cloudIcon(),
-              Container(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [_temprature(), _climate()],
-                ),
-              ),
+              _temprature(),
               _location(),
               SizedBox(
                 height: (heightDevice / 100) * 1,
@@ -75,13 +110,7 @@ class _HomePageState extends State<HomePage> {
           scrollDirection: Axis.vertical,
           itemCount: itmesWeek.length,
           itemBuilder: (context, index) {
-            return Container(
-                height: 50,
-                child: Card(
-                  child: Center(
-                    child: Text('${itmesWeek[index]}'),
-                  ),
-                ));
+            return Container(height: 50, child: CardWeeklyWeather());
           }),
     );
   }
@@ -112,13 +141,7 @@ class _HomePageState extends State<HomePage> {
           scrollDirection: Axis.horizontal,
           itemCount: itmesHour.length,
           itemBuilder: (context, index) {
-            return Container(
-                width: 50,
-                child: Card(
-                  child: Center(
-                    child: Text('${itmesHour[index]}'),
-                  ),
-                ));
+            return Container(width: 50, child: CardhourlyWeather(time: '${dataForecast.current}',));
           }),
     );
   }
@@ -134,9 +157,9 @@ class _HomePageState extends State<HomePage> {
           width: 10,
         ),
         Text(
-          '24.03.2022',
+          '${dataCurrent.location?.localtime}',
           style: TextStyle(fontSize: 18),
-        )
+        ),
       ],
     );
   }
@@ -149,34 +172,27 @@ class _HomePageState extends State<HomePage> {
           width: 10,
         ),
         Text(
-          'City',
+          '${dataCurrent.location?.region}',
           style: TextStyle(fontSize: 18),
-        )
+        ),
       ],
-    );
-  }
-
-  _climate() {
-    return Text(
-      'แจ่มใส',
-      style: TextStyle(fontSize: 40, fontWeight: FontWeight.w100),
     );
   }
 
   _temprature() {
     return Text(
-      '28',
+      '${dataCurrent.current?.tempC}',
       style: TextStyle(fontSize: 80, fontWeight: FontWeight.w100),
     );
   }
 
   _cloudIcon() {
     return Padding(
-      padding: const EdgeInsets.all(20),
-      child: Icon(
-        Icons.cloud,
-        size: 80,
-      ),
-    );
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Text('${dataCurrent.current?.condition!.icon}'),
+          ],
+        ));
   }
 }

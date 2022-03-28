c9bd08823c01faf62bdad7803f15fa89c7a99e58
boss29024 <boss29024@gmail.com>
2022-03-28 17:05:38 +0700

    CardhourlyWeather

---
+++ lib/widget/cardHourly.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class CardhourlyWeather extends StatelessWidget {
+  const CardhourlyWeather(
+      {Key? key, this.time = '16.39', this.temp = 0.0, this.icon = 'LOL'})
+      : super(key: key);
+
+  final String time;
+  final double temp;
+  final String icon;
+
+  @override
+  Widget build(BuildContext context) {
+    final heightDevice = MediaQuery.of(context).size.height;
+    return Container(
+      child: Card(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(time),
+            Text('${temp}'),
+            Text(icon),
+          ],
+        ),
+      ),
+    );
+  }
+}
